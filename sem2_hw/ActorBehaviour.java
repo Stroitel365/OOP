853d8a08c743bdda78041fab7b20a2eca0421050
Polina <philippova.polina@gmail.com>
2024-08-28 22:49:21 +0300

    add srm2_hw folder

---
+++ sem2_hw/ActorBehaviour.java
@@ -0,0 +1,8 @@
+package sem2_hw;
+
+public interface ActorBehaviour {
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean pickUpOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
